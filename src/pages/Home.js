332f76a84a93909e898f8e2ff96e24b097be74a3
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2023-11-29 01:00:35 -0500

    enlarged text on home page, need to add visuals to my pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,26 @@
 import './Home.css';
 import MyPic from "../Data/MyPicture.png";
-import MV_Resume from "../Data/MV_Resume.png";
-//import menuBar from "./Components/menuBar.js"
 
 export default function Home() {
     return (
         <div className="Home">
                 <backToTop/>
-                {/* <NavBar /> */}
-                <header>
-                {/* Your header content */}
-                </header>
                 <section id="Home">
                 {
                     <section id="home">
                     <img src={MyPic} className='my-pic' alt="My Picture"/> 
-                    <h1 className="welcome-text">
-                    <span class="first-word">Hello,</span><br></br>
-                    welcome to my site.
-                    </h1>
-                    <div className="additional-txt">
-                    <h2>My name is <span class="my-name">Maahir Vohra.</span></h2>
+                    <div className='Welcome-Message'>
+                        <h1 className="welcome-text">
+                            <span class="first-word">Hello,</span><br></br>
+                            welcome to my site.
+                        </h1>
+                        <div className="additional-txt">
+                            <h2>My name is <span class="my-name">Maahir Vohra.</span></h2>
+                        </div>
                     </div>
                     </section>
                 }
                 </section>
-                <section id="About Me">
-                {/* Portfolio section content */}
-                </section>
-                <section id="Resume">
-                <img src={MV_Resume} className="resume-pic" alt="My Resume"/>
-                </section>
-                {/* <menuBar/> */}
         </div>
-
-
     );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,12 @@
+import MV_Resume from "../Data/MV_Resume.png";
+import '../pages/Resume.css';
+
 export default function Resume () {
     return (
-        <h1>Resume</h1>
+        <div>
+        <section id="Resume">
+        <img src={MV_Resume} className="resume-pic" alt="My Resume"/>
+        </section>
+        </div>
     )
 };
