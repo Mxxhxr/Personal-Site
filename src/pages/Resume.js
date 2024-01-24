83629687a681574b190399fd7aa3b73efda4e35e
Maahir Vohra <Maahir Vohra>
2024-01-24 01:01:01 -0500

    resume again

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,15 +1,31 @@
+// // import MV_Resume from "../Data/MV_Resume.png";
+// import MyResume from "../Data/Maahir_Vohra_Resume.pdf"
+// import '../pages/Resume.css';
+
+// export default function Resume () {
+//     return (
+//         <div>
+//         <section id="Resume">
+//             <br/>
+//             <br/>
+//             <embed src={MyResume} type="application/pdf" width="850" height="675"/>
+//         </section>
+//         </div>
+//     )
+// };
+
 // import MV_Resume from "../Data/MV_Resume.png";
-import MyResume from "../Data/Maahir_Vohra_Resume.pdf"
+import MyResume from "../Data/Maahir_Vohra_Resume.pdf";
 import '../pages/Resume.css';
 
-export default function Resume () {
+export default function Resume() {
     return (
         <div>
-        <section id="Resume">
-            <br/>
-            <br/>
-            <embed src={MyResume} type="application/pdf" width="850" height="675"/>
-        </section>
+            <section id="Resume">
+                <br />
+                <br />
+                <embed src={MyResume} type="application/pdf" width="100%" height="100%" className="pdf-embed" />
+            </section>
         </div>
-    )
-};
+    );
+}
