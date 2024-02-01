de4a554664eca3f7052a6e5f3924cc5788998d05
Maahir Vohra <Maahir Vohra>
2024-01-31 22:05:03 -0500

    Updated Resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,21 +1,5 @@
-// // import MV_Resume from "../Data/MV_Resume.png";
-// import MyResume from "../Data/Maahir_Vohra_Resume.pdf"
-// import '../pages/Resume.css';
-
-// export default function Resume () {
-//     return (
-//         <div>
-//         <section id="Resume">
-//             <br/>
-//             <br/>
-//             <embed src={MyResume} type="application/pdf" width="850" height="675"/>
-//         </section>
-//         </div>
-//     )
-// };
-
 // import MV_Resume from "../Data/MV_Resume.png";
-import MyResume from "../Data/Maahir_Vohra_Resume.pdf";
+import MyResume from "../Data/MyResume.pdf";
 import '../pages/Resume.css';
 
 export default function Resume() {
